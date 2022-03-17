b02779d7d94c11ce3d131e05d689d475480754d2
andersoncsiqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2022-03-17 20:02:22 -0300

    up

--- app.js
+++ app.js
@@ -3,32 +3,46 @@ const coinOne = document.querySelector('[data-js="coinOne"]')
 const coinTwo = document.querySelector('[data-js="coinTwo"]')
 const coins = ['USD','EUR','GBP','JPY','CAD','NZD','CHF','AUD']
 
-let url = 'https://economia.awesomeapi.com.br/json/daily/AUD-USD/30'
+
+
 
 const putCoins = () => {
     let allCoins = ''
     coins.forEach(coin => {
         allCoins += `<option>${coin}</option>`
     })
-
     coinOne.innerHTML = allCoins
     coinTwo.innerHTML = allCoins
 }
 
+const priceClosed = (cotacoes) => {
+    
+
+    console.log(Array.from(cotacoes).map((price, index) => {
+        console.log(price[index]['bid'])
+    }))
+
+}
+    putCoins()
+    
 
 
 
-
-/*fetch('https://economia.awesomeapi.com.br/json/daily/AUD-USD/30')
+    setInterval(()=>{
+        let coinOneSelected = coinOne.value
+        let coinTwoSelected = coinTwo.value
+        let url = `https://economia.awesomeapi.com.br/json/daily/${coinOneSelected}-${coinTwoSelected}/30`
+        
+fetch(url)
     .then((resp)=> {
        return resp.json()
     }).then(cotacoes =>  {
-        console.log(cotacoes)
+       priceClosed(cotacoes)
     })
     .catch((err)=> {
-        console.log(err)
-    }) */
+        console.log(err)  
+    })
+    
+    },1000) 
 
-    putCoins()
-
-    setInterval(()=>{console.log(coinOne.value)},1000) 
+    
