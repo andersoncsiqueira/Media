f9c993066e353c38e2b4d7a361617cb57e78fbcb
andersoncsiqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2022-03-17 17:07:11 -0300

    array for optoins

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const moedas = ['USD','EUR','GBP','JPY','CAD','NZD','CHF','AUD']
+
+const coinOne = document.querySelector('[data-js="coinOne"]')
+const coinTwo = document.querySelector('[data-js="coinTwo"]')
+const coins = ['USD','EUR','GBP','JPY','CAD','NZD','CHF','AUD']
+
+let url = 'https://economia.awesomeapi.com.br/json/daily/AUD-USD/30'
+
+const putCoins = () => {
+    let allCoins = ''
+    coins.forEach(coin => {
+        allCoins += `<option>${coin}</option>`
+    })
+
+    coinOne.innerHTML = allCoins
+    coinTwo.innerHTML = allCoins
+}
 
 
-fetch('https://economia.awesomeapi.com.br/json/daily/AUD-USD/30')
+
+
+
+/*fetch('https://economia.awesomeapi.com.br/json/daily/AUD-USD/30')
     .then((resp)=> {
        return resp.json()
     }).then(cotacoes =>  {
@@ -9,4 +27,8 @@ fetch('https://economia.awesomeapi.com.br/json/daily/AUD-USD/30')
     })
     .catch((err)=> {
         console.log(err)
-    })
+    }) */
+
+    putCoins()
+
+    setInterval(()=>{console.log(coinOne.value)},1000) 
