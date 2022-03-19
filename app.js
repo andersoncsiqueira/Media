46fb3aeeb27c770cdf4bafdb4b3e07aae2b98111
andersoncsiqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2022-03-19 12:26:27 -0300

    parameters

--- app.js
+++ app.js
@@ -1,79 +1,34 @@
 
 const coinOne = document.querySelector('[data-js="coinOne"]')
 const coinTwo = document.querySelector('[data-js="coinTwo"]')
-const table = document.querySelector('[data-js="table"]')
-const coins = ['USD','EUR','GBP','JPY','CAD','NZD','CHF','AUD','BRL']
+const numbersDay = document.querySelector('[data-js="days"]')
+const button = document.querySelector('[data-js="buttonFirstSearch"]')
+const infoOne = document.querySelector('[data-js="tableInfo"]')
+const coins = ['','USD','EUR','GBP','JPY','CAD','NZD','CHF','AUD','BRL']
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,
+    23,24,25,26,27,28,29,30]
 
-const putCoins = () => {
+const setSelecters = (array,element) => {
     let allCoins = ''
-    coins.forEach(coin => {
+    array.forEach(coin => {
         allCoins += `<option>${coin}</option>`
     })
-    coinOne.innerHTML = allCoins
-    coinTwo.innerHTML = allCoins
+    element.innerHTML = allCoins
+    
 }
-putCoins()
 
-const priceClosed = (cotacoes) => {
-    let tableHTML = ""
-    let highs = cotacoes.forEach(item => {
+button.addEventListener('click', ()=> {
+    console.log(coinOne.value,coinTwo.value,numbersDay.value)
+})
 
-        tableHTML += `<li>${item.high}<li>`
-    
-    })
 
-        console.log(tableHTML)
-    table.innerHTML =  tableHTML
 
-   /* cotacoes.reduce((acc,item)=> acc + item.high
-        
-    ,0)
 
-    cotacoes.reduce((acc,price)=> {
-         let prices  = Number(price.bid)
-         let accs = Number(acc)
-         accs += prices
-        
-         console.log(accs)
-    },0)*/
-   
-      
-} 
-    putCoins()
+setSelecters(coins,coinOne)
+setSelecters(coins,coinTwo)
+setSelecters(numbers,numbersDay)
+ 
+
     
 
-
-
-
-    setInterval(()=>{
-        let coinOneSelected = coinOne.value
-        //let coinTwoSelected = coinTwo.value
-        let url = `https://api.exchangerate.host/timeseries?start_date=2020-01-01&end_date=2020-02-04&base=${coinOneSelected}`
-        
-fetch(url)
-    .then((resp)=> {
-       return resp.json()
-    }).then(cotacoes =>  {
-     //priceClosed(cotacoes)
-     // const {bid} = cotacoes[0]
-     // console.log(bid)
-     console.log(cotacoes)
-    })
-    .catch((err)=> {
-        console.log(err)  
-    })
-    
-    },2000) 
-
-    /*var requestURL = 'https://api.exchangerate.host/timeseriesstart_date=2020-01-01&end_date=2020-02-04?base=GBP';
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-
-request.onload = function() {
-  var response = request.response;
-  console.log(response);
-}
-*/
-    
+  
